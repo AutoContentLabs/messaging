de50e50e2bed703ebab949e5472a7ed9e3b1407e
Quenteon <quanteonquantumelysium@gmail.com>
2024-11-27 09:32:31 +0000

    add multiple exporter

--- src/utils/Telemetry.js
+++ src/utils/Telemetry.js
@@ -2,25 +2,41 @@ const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
 const { Resource } = require('@opentelemetry/resources');
 const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { trace } = require('@opentelemetry/api');
 const config = require('../transporters/config');
 
 class Telemetry {
   constructor() {
     const serviceName = `${config.GROUP_ID}-${config.MESSAGE_SYSTEM}`;
-    this.exporter = new ZipkinExporter({
+    
+    // Zipkin Exporter
+    this.zipkinExporter = new ZipkinExporter({
       serviceName,
-      url: 'http://zipkin:9411/api/v2/spans',
+      url: 'http://zipkin:9411/api/v2/spans',  // Zipkin URL
     });
 
+    // Jaeger Exporter
+    this.jaegerExporter = new JaegerExporter({
+      serviceName,
+      endpoint: 'http://jaeger:5775/api/traces',  // Jaeger endpoint
+    });
+
+    // Tracer Provider
     this.provider = new NodeTracerProvider({
       resource: new Resource({
         'service.name': serviceName,
       }),
     });
 
-    this.provider.addSpanProcessor(new SimpleSpanProcessor(this.exporter));
+    // Add both Zipkin and Jaeger span processors
+    this.provider.addSpanProcessor(new SimpleSpanProcessor(this.zipkinExporter));
+    this.provider.addSpanProcessor(new SimpleSpanProcessor(this.jaegerExporter));
+
+    // Register the provider
     this.provider.register();
+
+    // Get the tracer
     this.tracer = trace.getTracer(serviceName);
   }
 
@@ -29,10 +45,10 @@ class Telemetry {
   }
 
   /**
- * Converts a model object to Zipkin-compatible tags dynamically.
- * @param {Object} model - The model object containing dynamic data.
- * @returns {Object} - A set of tags that can be added to the Zipkin span.
- */
+   * Converts a model object to Zipkin-compatible tags dynamically.
+   * @param {Object} model - The model object containing dynamic data.
+   * @returns {Object} - A set of tags that can be added to the Zipkin span.
+   */
   convertModelToTags(model) {
     const tags = {};
 
