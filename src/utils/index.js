c5fe45b6c5ff37d2961e5896928558c5c5e3ed45
Quenteon <quanteonquantumelysium@gmail.com>
2024-12-03 10:02:59 +0000

    new utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,12 @@
-const logger = require("./logger")
-const helper = require("./helper")
-const instance = require("./instance")
-const retry = require("./retry")
-const transformer = require("./transformer")
-const fileWriter = require("./fileWriter")
-const telemetry = require("./Telemetry")
+const logger = require("./logger");
+const helper = require("./helper");
+const instance = require("./instance");
+const retry = require("./retry");
+const transformer = require("./transformer");
+const fileWriter = require("./fileWriter");
+const telemetry = require("./Telemetry");
+const batchSize = require("./batchSize");
+const progress = require("./progress");
 module.exports = {
     logger,
     helper,
@@ -12,5 +14,7 @@ module.exports = {
     retry,
     transformer,
     fileWriter,
-    telemetry
-}
+    telemetry,
+    batchSize,
+    progress,
+};
